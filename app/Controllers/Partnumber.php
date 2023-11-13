bf1a6b97a6cfe0fd1c99f65892b5de7264814306
Stevanus Ertito <82057016+Tevan13@users.noreply.github.com>
2023-11-13 10:31:50 +0700

    session login for each controller

--- app/Controllers/Partnumber.php
+++ app/Controllers/Partnumber.php
@@ -7,17 +7,22 @@ use App\Controllers\BaseController;
 
 class Partnumber extends BaseController
 {
-    public function __construct() {
+    public function __construct()
+    {
         $this->PartnumberModel = new PartnumberModel();
     }
 
     public function index()
     {
+        if (session()->get('tb_user') == null) {
+            return redirect()->to('/login');
+        }
         $data['parts'] = $this->PartnumberModel->findAll();
         return view('master/partnumber', $data);
     }
 
-    public function store() {
+    public function store()
+    {
         $data = $this->request->getPost();
         $store = $this->PartnumberModel->protect(false)->insert($data, false);
 
@@ -27,14 +32,16 @@ class Partnumber extends BaseController
         return redirect()->route('master-part');
     }
 
-    public function update($id) {
+    public function update($id)
+    {
         $data = $this->request->getPost();
         unset($data['_method']);
         $this->PartnumberModel->protect(false)->update($id, $data);
         return redirect()->route('master-part');
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $this->PartnumberModel->where('idPartNo', $id)->delete();
         return redirect()->route('master-part');
     }
