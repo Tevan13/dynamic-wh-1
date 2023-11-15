5d9f138765f44571b3f316cdf5d0cacb0b40fcec
Rayhaaa <rayhanayasmin.2021@student.uny.ac.id>
2023-11-15 16:07:36 +0700

    fix navbar

--- app/Views/DashboardView.php
+++ app/Views/DashboardView.php
@@ -1,50 +1,52 @@
 <?= $this->extend('layout/index'); ?>
 <?= $this->section('content'); ?>
-<div class="container">
+<div class="container-fluid p-3">
     <h2>Dashboard</h2>
-    <div class="">
-        <a href="<?= base_url('/scan-ci') ?>" class="btn btn-primary">
-            <div>
-                <div class="card-body">
-                    <h5 class="card-title">scan</h5>
+    <div class="card mb-3 mt-2 p-3">
+        <div class="">
+            <a href="<?= base_url('/scan-ci') ?>" class="btn btn-primary">
+                <div>
+                    <div class="card-body">
+                        <h5 class="card-title">scan</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
-        <a href="<?= base_url('/information-rak') ?>" class="btn btn-primary">
-            <div>
-                <div class="card-body">
-                    <h5 class="card-title">informasi rak</h5>
+            </a>
+            <a href="<?= base_url('/informationController') ?>" class="btn btn-primary">
+                <div>
+                    <div class="card-body">
+                        <h5 class="card-title">informasi rak</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
-        <a href="history/" class="btn btn-primary">
-            <div>
-                <div class="card-body">
-                    <h5 class="card-title">history</h5>
+            </a>
+            <a href="history/" class="btn btn-primary">
+                <div>
+                    <div class="card-body">
+                        <h5 class="card-title">history</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
-        <a href="/master-part" class="btn btn-primary">
-            <div>
-                <div class="card-body">
-                    <h5 class="card-title">master part number</h5>
+            </a>
+            <a href="/master-part" class="btn btn-primary">
+                <div>
+                    <div class="card-body">
+                        <h5 class="card-title">master part number</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
-        <a href="/master-user" class="btn btn-primary">
-            <div>
-                <div class="card-body">
-                    <h5 class="card-title">master User</h5>
+            </a>
+            <a href="/master-user" class="btn btn-primary">
+                <div>
+                    <div class="card-body">
+                        <h5 class="card-title">master User</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
-        <a href="<?= base_url('/master_rak') ?>" class="btn btn-primary">
-            <div>
-                <div class="card-body">
-                    <h5 class="card-title">master rak</h5>
+            </a>
+            <a href="<?= base_url('/master_rak') ?>" class="btn btn-primary">
+                <div>
+                    <div class="card-body">
+                        <h5 class="card-title">master rak</h5>
+                    </div>
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
     </div>
 </div>
 <?= $this->endSection(); ?>
